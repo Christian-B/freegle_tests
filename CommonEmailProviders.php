c0c7028e8c24cc7c1cd061ecf31db0e365db8b4c
Christian Brenninkmeijer <github@brenn.co.uk>
2016-11-29 10:53:12 +0000

    Added recommend domian to DomainChecker

---
+++ CommonEmailProviders.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace freegle\CommonEmailProviders;
+
+const COM_DOMAINS =array (
+    "yahoo.com",
+    "hotmail.com",
+    "gmail.com",
+    "btinternet.com",
+    "ntlworld.com",
+    "googlemail.com",
+    "ymail.com",
+    "rocketmail.com",
+    "aol.com",
+    "sky.com",
+    "msn.com",
+    "outlook.com",
+    "btopenworld.com",
+    "talk21.com",
+    "live.com",
+    "icloud.com",
+    "me.com",
+    "virginmedia.com",
+    "mail.com",
+    "mac.com",
+    "dsl.pipex.com",
+    "gmx.com",
+    "btconnect.com",
+    "supanet.com",
+    "onetel.com",
+    "bigfoot.com",
+    "email.com",
+    "lycos.com");
+
+const CO_UK_DOMAINS = array (
+    "yahoo.co.uk",
+    "hotmail.co.uk",
+    "live.co.uk",
+    "tiscali.co.uk",
+    "aol.co.uk",
+    "o2.co.uk",
+    "gmx.co.uk",
+    "blueyonder.co.uk",
+    "zen.co.uk",
+    "homecall.co.uk");
+
+const NET_DOMAINS = array(
+    "talktalk.net",
+    "virgin.net",
+    "fsmail.net",
+    "tesco.net",
+    "uwclub.net",
+    "lineone.net",
+    "orange.net");
+
+const OTHER_DOMAINS = array(
+    "wp.pl",
+    "yahoo.co.in",
+    "yahoo.com.au",
+    "phonecoop.coop",
+    "doctors.org.uk",
+    "yahoo.fr",
+    "yahoo.ca",
+    "homecall.co.uk",
+    "yahoo.ie");
+
+
--- DomainChecker.php
+++ DomainChecker.php
@@ -2,6 +2,10 @@
 
 namespace freegle\DomainChecker;
 
+require_once("CommonEmailProviders.php");
+require_once("DamerauLevenshtein.php");
+require_once("UrlTopLevel.php");
+
 const UNKNOW = "unknown";
 
 class DomainChecker
@@ -37,6 +41,91 @@ class DomainChecker
         return UNKNOW;
     }
 
+    function recommendedDomain($domain, $tolerance)
+    {
+        $topLevel = $this->getOrignalTopLevel();
+        switch ($topLevel) {
+            case \freegle\UrlTopLevel\UK_COMMERCIAL:
+                $recommended = $this->recommendedDomainFromList($domain, \freegle\CommonEmailProviders\CO_UK_DOMAINS, $tolerance);
+                break;
+            case \freegle\UrlTopLevel\COMMERCIAL:
+                $recommended = $this->recommendedDomainFromList($domain, \freegle\CommonEmailProviders\COM_DOMAINS, $tolerance);
+                break;
+            case \freegle\UrlTopLevel\NETWORK:
+                $recommended = $this->recommendedDomainFromList($domain, \freegle\CommonEmailProviders\NET_DOMAINS , $tolerance);
+                break;
+            case \freegle\UrlTopLevel\UK_ACADEMIC:
+                $recommended = $this->acUkRecommendation($domain, $tolerance);
+                break;
+            case UNKNOW:
+                #Bad toplevel so don't check just others
+                $recommended = false;
+                break;
+            default:
+                $recommended = $this->recommendedDomainFromList($domain, \freegle\CommonEmailProviders\OTHER_DOMAINS , $tolerance);
+        }
+        if ($recommended != false){
+            return $recommended;
+        }
+        return $this->recommendedDomainFromList($domain, $this->allDomains(), $tolerance);
+
+    }
+
+    function acUkRecommendation($domain, $tolerance)
+    {
+        $parts = explode(".", $domain);
+        $count = count($parts);
+        if ($count == 3){
+            return $this->recommendedDomainFromList($domain, \freegle\UrlTopLevel\AC_UK_THIRD, $tolerance);
+        } elseif  ($count < 3) {
+            //$domain is exactly ac.uk so match is impossible
+            return false;
+        } else {
+            //Many uni have sub domains like  @cs.man.ac.uk
+            //So check the last three biits and assume the first is correct
+            $lastThree = $parts[$count-3] . "." . $parts[$count-2] . "." . $parts[$count-1];
+            $recommended = $this->recommendedDomainFromList($lastThree, \freegle\UrlTopLevel\AC_UK_THIRD, $tolerance);
+            //now add the first bits back in
+            if ($recommended == $lastThree){
+                $recommnded = $domain;
+            } elseif ($recommended != false){
+                for ($i = 0; $i < $count -3; $i++) {
+                    echo $i . $parts[$i] . "<br>";
+                    $recommended = $parts[$i] . "." .  $recommended;
+                }
+            }
+            return $recommended;
+        }
+    }
+
+    function recommendedDomainFromList($domain, $list, $tolerance)
+    {
+        if (in_array($domain , $list)){
+            return $domain;
+        }
+        $bestFind = false;
+        $bestValue = $tolerance + 1;
+        foreach($list as $possible){
+            $checker = new \Oefenweb\DamerauLevenshtein\DamerauLevenshtein($domain, $possible);
+            $difference =  $checker->getSimilarity();
+            if ($difference < $bestValue){
+                $bestValue = $difference;
+                $bestFind = $possible;
+            }
+        }
+        return $bestFind;
+    }
+
+    function allDomains()
+    {
+        return array_merge(\freegle\CommonEmailProviders\CO_UK_DOMAINS,
+                           \freegle\CommonEmailProviders\COM_DOMAINS,
+                           \freegle\CommonEmailProviders\NET_DOMAINS,
+                           \freegle\CommonEmailProviders\OTHER_DOMAINS,
+                           \freegle\UrlTopLevel\AC_UK_THIRD);
+    }
+
+
     /**
      * @return mixed
      */
@@ -61,6 +150,9 @@ class DomainChecker
      */
     public function getRecommendedDomain()
     {
+        if (!isset($this->recommended_domain)) {
+            $this->recommended_domain = $this->recommendedDomain($this->getOriginalDomain(), 2);
+        }
         return $this->recommended_domain;
     }
 

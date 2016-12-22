fe1413ebfb372dc095c37bb31e4e935d639dc003
Christian Brenninkmeijer <github@brenn.co.uk>
2016-12-22 12:37:11 +0000

    Imporved Script creator for extra level

---
+++ TopLevelExtra.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace freegle\TopLevelExtra;
+
+/*
+ *Automatically generated file.
+ *Generated using DataCreator.php
+ */
+
+const DOMAINS = array(
+	"com" => array (
+		"rr.com",
+		"zzn.com",
+		"blackberry.com",
+		"plus.com",
+		"uk.com",
+		"f2s.com",
+		"eu.com",
+		"gb.com",
+		"u-net.com",
+		"freeuk.com",
+		"vispa.com",
+		"waitrose.com",
+		"entadsl.com",
+	),
+	"org" => array (
+		"oxon.org",
+	),
+	"co.uk" => array (
+		"fsnet.co.uk",
+		"freeserve.co.uk",
+		"demon.co.uk",
+		"karoo.co.uk",
+		"eclipse.co.uk",
+		"wanadoo.co.uk",
+		"free-online.co.uk",
+		"orangehome.co.uk",
+		"myzen.co.uk",
+		"fslife.co.uk",
+		"f9.co.uk",
+		"ndo.co.uk",
+		"idps.co.uk",
+		"fsbusiness.co.uk",
+		"clara.co.uk",
+		"force9.co.uk",
+		"mail1.co.uk",
+		"fsworld.co.uk",
+		"adsl24.co.uk",
+		"nildram.co.uk",
+		"worldonline.co.uk",
+		"globalnet.co.uk",
+		"gotadsl.co.uk",
+	),
+	"net" => array (
+		"uk.net",
+		"comcastbiz.net",
+		"clara.net",
+		"screaming.net",
+		"go-plus.net",
+	),
+	"au" => array (
+		"nsw.au",
+	),
+	"ac.uk" => array (
+		"ed.ac.uk",
+		"gla.ac.uk",
+		"ox.ac.uk",
+		"cam.ac.uk",
+		"oxford.ac.uk",
+		"ucl.ac.uk",
+	),
+	"ca" => array (
+		"on.ca",
+	),
+	"edu" => array (
+		"harvard.edu",
+		"umass.edu",
+	),
+	"it" => array (
+		"191.it",
+	),
+	"jp" => array (
+		"odn.jp",
+		"att.jp",
+	),
+	"gov.uk" => array (
+		"gsi.gov.uk",
+	),
+	"fm" => array (
+		"fastmail.fm",
+	),
+	"cc" => array (
+		"co.cc",
+	),
+);
+
+const EXTRA_DEPTHS = array(
+	"com"=>3,
+	"org"=>3,
+	"co.uk"=>4,
+	"net"=>3,
+	"au"=>4,
+	"ac.uk"=>4,
+	"ca"=>3,
+	"edu"=>3,
+	"it"=>3,
+	"jp"=>4,
+	"gov.uk"=>4,
+	"fm"=>3,
+	"cc"=>3,
+);
